116a9ca80a1242115dfd7e9a56bcf0077749ddf7
maciej-or <maciej.orlowski@scalac.io>
2019-05-20 12:04:39 +0200

    DOM events

--- client/src/app/chat-input/chat-input.component.ts
+++ client/src/app/chat-input/chat-input.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { ChatService, ConnectionState } from '../chat.service';
+import { fromEvent } from 'rxjs';
+import { throttle, throttleTime, debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-chat-input',
   templateUrl: './chat-input.component.html',
   styleUrls: ['./chat-input.component.scss']
 })
-export class ChatInputComponent implements OnInit {
+export class ChatInputComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('chatInput') chatInput: ElementRef;
 
   message: string;
 
@@ -18,6 +22,23 @@ export class ChatInputComponent implements OnInit {
     this.chatService.connectionState$.subscribe(state => {
       this.isConnected = (state === ConnectionState.ok);
     });
+  }
+
+  ngAfterViewInit() {
+    const keyDown$ = fromEvent(this.chatInput.nativeElement, 'keydown');
+    keyDown$.pipe(
+      throttleTime(600)
+    )
+    .subscribe(() => {
+      this.chatService.sendTypingNotification();
+    });
+
+    // const keyUp$ = fromEvent(this.chatInput.nativeElement, 'keyup');
+    // keyUp$.pipe(
+    //   debounceTime(1000)
+    // ).subscribe(
+    //   // send notify 'stop typing'
+    // );
   }
 
   sendMessage() {
